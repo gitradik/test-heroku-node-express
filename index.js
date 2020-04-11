2391b30bc20e61b7b12d4af0b16e965244ffc75d
gitradik <rminzp17@gmail.com>
2020-04-11 10:25:07 +0300

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 80;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000);
+app.listen(PORT);
