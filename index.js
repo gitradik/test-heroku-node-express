997601a680330846a8c176cc154664c9ce046563
gitradik <rminzp17@gmail.com>
2020-04-11 10:42:47 +0300

    install dotenv package

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv');
+dotenv.config(); 
+
 const PORT = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
