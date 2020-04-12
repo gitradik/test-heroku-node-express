f410d71ceebbf60735a05ad90faa2b386ebe5b6e
gitradik <rminzp17@gmail.com>
2020-04-12 17:37:16 +0300

    add db - 'hobby-dev' & add table - 'test_table'

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ dotenv.config();
 
 const PORT = process.env.PORT || 80;
 
+const pool = require('./db');
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World' + req.query.var);
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 app.listen(PORT);
