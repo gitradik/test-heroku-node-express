b8798be797a2af9b7dd0ebe0b2d244e5ad8c7f67
gitradik <rminzp17@gmail.com>
2020-04-14 23:21:45 +0300

    add express body parser

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-
 dotenv.config(); 
 
 const PORT = process.env.PORT || 80;
 
+app.use(express.json());
+
 const { pool } = require('./db');
 
 app.get('/', (req, res) => {
-  
   const { name } = req.query;
   const message = `Hello ${name ? name : 'World'}`;
   res.send(message);
+});
+
+app.put('/', (req, res) => {
+  res.send(req.body); 
 });
 
 app.get('/db', async (req, res) => {
