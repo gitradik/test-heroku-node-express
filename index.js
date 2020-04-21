1a406111c0467037039c57c914c59cd02b5a6628
Lidogeneration <42538869+Lidogeneration@users.noreply.github.com>
2020-04-21 08:24:29 +0300

    gallery

--- index.js
+++ index.js
@@ -30,38 +30,38 @@ app.get('/test', (req, res) => {
   res.send(JSON.stringify(message));
 });
 
-
+let message = {
+  version: "v2",
+  content: {
+    messages: [
+      {
+        type: "cards",
+        elements: [
+          {
+            title: "Card title",
+            subtitle: "card text",
+            image_url: "https://manybot-thumbnails.s3.eu-central-1.amazonaws.com/ca/xxxxxxzzzzzzzzz.png",
+            action_url: "https://manychat.com",
+            buttons: []
+          },
+          {
+            title: "Card title 2",
+            subtitle: "card text 2",
+            image_url: "https://manybot-thumbnails.s3.eu-central-1.amazonaws.com/ca/xxxxxxzzzzzzzzz.png",
+            action_url: "https://manychat.com",
+            buttons: []
+          }
+        ],
+        image_aspect_ratio: "horizontal"
+      }
+    ],
+    actions: [],
+    quick_replies: []
+  }
+};
 
 app.get('/bst', (req, res) => {
-  const message = {
-    version: "v2",
-    content: {
-      messages: [
-        {
-          type: "cards",
-          elements: [
-            {
-              title: "Card title",
-              subtitle: "card text",
-              image_url: "https://manybot-thumbnails.s3.eu-central-1.amazonaws.com/ca/xxxxxxzzzzzzzzz.png",
-              action_url: "https://manychat.com",
-              buttons: []
-            },
-            {
-              title: "Card title 2",
-              subtitle: "card text 2",
-              image_url: "https://manybot-thumbnails.s3.eu-central-1.amazonaws.com/ca/xxxxxxzzzzzzzzz.png",
-              action_url: "https://manychat.com",
-              buttons: []
-            }
-          ],
-          image_aspect_ratio: "horizontal"
-        }
-      ],
-      actions: [],
-      quick_replies: []
-    }
-  }
+
   res.send(JSON.stringify(message));
 });
 
