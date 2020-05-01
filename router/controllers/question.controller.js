e980baf854ad67bb253472935965d1b631c9ddb9
gitradik <rminzp17@gmail.com>
2020-05-01 10:09:55 +0300

    log...

--- router/controllers/question.controller.js
+++ router/controllers/question.controller.js
@@ -1,6 +1,8 @@
 const questions = require('../../utils/questions/qv.json');
 
 module.exports.getQuestionByCount = (req, res) => {  
+  console.log('>>>>>>', req.body);
+
   const qv = questions.find(q => q.key === req.body.count);
   if (qv) { 
     res.send(qv);
